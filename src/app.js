cc18becd7ef79d6c11476994d6d803d28ad4ba1d
Roman Himadinov <roman@brandetize.com>
2017-11-29 21:15:51 -0800

    Added project

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import { person } from './lib';
+
+console.log(person.name);
+
+async function getPosts () {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+  const data = await response.json();
+
+  return data;
+}
+
+getPosts().then(posts => console.log(posts));
---
+++ src/lib.js
@@ -0,0 +1,5 @@
+export const person = {
+  name: "James Bond",
+  location: "San Diego",
+  age: 45
+}
